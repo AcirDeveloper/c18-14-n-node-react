2f7c81c5374fe680010689e87af2e4fd3cf7fbfe
Sonny <spimentelm1201@gmail.com>
2024-06-05 10:03:39 -0500

    feat: cors added

--- server/src/app.ts
+++ server/src/app.ts
@@ -3,9 +3,20 @@ import 'reflect-metadata';
 import { AppDataSource } from './database/data-source'; 
 import routes from './routes/index.routes';
 import initializeRoles from './scripts/initializeRoles';
+import cors from 'cors';
 
 const app = express();
+const FRONTEND_URL = process.env.FRONTEND_URL;
+
+const corsOptions = {
+  origin: ['*', FRONTEND_URL],
+  credentials: true,
+  methods: ['GET', 'POST', 'PATCH', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+};
+
 app.use(express.json());
+app.use(cors(corsOptions));
 
 const initializeApp = async () => {
   try {
