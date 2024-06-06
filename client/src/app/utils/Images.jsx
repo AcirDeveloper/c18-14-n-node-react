287d7657c8de29a4906701c198464a738823e8fe
pitt <roypalominor28@gmail.com>
2024-06-06 07:07:06 -0500

    create Alert componet update SaldoCard add images alertVerification - saldo1

--- client/src/app/utils/Images.jsx
+++ client/src/app/utils/Images.jsx
@@ -2,7 +2,9 @@ const images = {
 	eyeOn: '/icons/eye-on.svg',
 	eyeOff: '/icons/eye-off.svg',
 	authImage: 'images/auth-image.png',
-	terminos: 'images/terminos.png'
+	terminos: 'images/terminos.png',
+	saldo1: 'icons/saldo1.svg',
+	alertVerification: 'icons/alertVerification.svg',
 }
 
 export default images
