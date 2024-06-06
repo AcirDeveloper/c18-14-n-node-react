64d851c8561ea90876f69b8a6a7af6d240b6bd29
Diaz027 <dami27cito@gmail.com>
2024-06-06 09:15:06 -0300

    Mis Actividades

--- client/src/app/page.js
+++ client/src/app/page.js
@@ -1,3 +1,4 @@
+import MisActividades from "@/components/MisActividades/MisActividades";
 import SaldoCard from "@/components/SaldoCard/SaldoCard";
 
 export default function Home() {
@@ -5,6 +6,7 @@ export default function Home() {
 		<>
 			<h1>HOME</h1>
 			<SaldoCard></SaldoCard>
+			<MisActividades />
 		</>
 	)
 }
---
+++ client/src/components/MisActividades/MisActividades.jsx
@@ -0,0 +1,15 @@
+const MisActividades = () => {
+    return (
+        <nav className="bg-sky-100 h-72 w-[50%] mt-6 ml-[18%] rounded-2xl border-solid border-l-2 border-b-4 border-slate-400">
+            <ul className="bg-[#112F63] flex justify-between px-4 py-2 text-white rounded-t-2xl">
+                <li>Fecha</li>
+                <li>Contacto</li>
+                <li>Transaccion</li>
+                <li>Estado</li>
+                <li>Cantidad</li>
+                <li>Detalle</li>
+            </ul>
+        </nav>
+    )
+}
+export default MisActividades
